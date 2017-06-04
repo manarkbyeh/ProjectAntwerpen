6e323ecbd8a4329353ea3454d75a8de65c4d283d
manarkbyeh <manar.kbyeh@student.kdg.be>
2017-06-04 15:40:36 +0200

    project

---
+++ laravel/resources/lang/nl/main.php
@@ -0,0 +1,12 @@
+<?php
+return [
+'en'=>'Engels',
+'nl'=>'Nederlands',
+'Contact'=>'Contact NL',
+'successfule_sent'=>'Jouw bericht werd succesvol geplaatst',
+
+'successfule_email'=>'Jouw bericht werd succesvol verstuurd',
+'successfule_edit'=>'Jouw bericht werd succesvol gewijzigd',
+
+'successfule_delete'=>'Jouw bericht werd succesvol verwijderd',
+];
